6fcb68c3a81d8282db265c0352e390456148ca70
nikhilsonu-23 <nikhil.m@Technovert.com>
2024-07-13 19:56:46 +0530

    swagger in production

--- WebSocketPrac/Program.cs
+++ WebSocketPrac/Program.cs
@@ -17,11 +17,11 @@ internal class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
+        // if (app.Environment.IsDevelopment())
+        // {
             app.UseSwagger();
             app.UseSwaggerUI();
-        }
+        // }
 
         app.UseHttpsRedirection();
 
